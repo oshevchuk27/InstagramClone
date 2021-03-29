6188e637e592f250697ef8ef3212e7aeebdfb1ef
oshevchuk27 <oshevchuk@brynmawr.edu>
2021-03-29 04:51:25 -0400

    added some new features

--- app/src/main/java/com/example/instagramclone/ParseApplication.java
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -3,12 +3,17 @@ package com.example.instagramclone;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
+
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("MEtZLJUNvVapP3wa82u5SafsoKUqYc1LFjGN4VCh")
